b4f220aa46e2986b6d87c330efa335a82381c165
DayKui <15658052167@163.com>
2021-02-28 23:18:08 +0800

    删除gameserver

--- moba_game_server/moba_server/netbus/tp_protocol.h
+++
@@ -1,5 +0,0 @@
-#ifndef _TP_PROTOCOL_H_
-#define _TP_PROTOCOL_H_
-class tp_protocol {public:	static bool read_header(unsigned char* data, int data_len, int* pkg_size, int* out_header_size);	static unsigned char* package(const unsigned char* raw_data, int len, int* pkg_len);	static void release_package(unsigned char* tp_pkg);};
-
-#endif // !_TP_PROTOCOL_H_
